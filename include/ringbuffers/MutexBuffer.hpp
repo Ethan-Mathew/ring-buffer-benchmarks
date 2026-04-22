4cbdb37af9fefe5585948d3b85fa9f27d93fbb5f
Ethan Mathew <ethan.mathew55555@gmail.com>
2026-04-22 00:31:36 -0400

    refactor: conform MutexBuffer public API to SPSC style

--- include/ringbuffers/MutexBuffer.hpp
+++ include/ringbuffers/MutexBuffer.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <cassert>
 #include <condition_variable>
 #include <cstddef>
 #include <memory>
 #include <mutex>
 #include <optional>
+#include <stdexcept>
+#include <type_traits>
 #include <utility>
 
 namespace ringbuffers
@@ -15,8 +18,14 @@ class MutexBuffer
 {
 public:
     explicit MutexBuffer(std::size_t bufferSize)
-        : capacity_{bufferSize}
     {
+        if (bufferSize < 1)
+        {
+            throw std::invalid_argument("MutexBuffer capacity must be greater than or equal to 1");
+        }
+
+        capacity_ = bufferSize;
+
         buffer_ = std::allocator_traits<Allocator>::allocate(allocator_, bufferSize);
     }
 
@@ -39,34 +48,75 @@ public:
     }
 
     template <typename... Args>
-    void push(Args&&... item)
+    void emplace(Args&&... args)
     {
         std::unique_lock<std::mutex> lock{mutex_};
 
-        cvFull_.wait(lock, [this]{return (count_ != capacity_) || closed_;});
+        cvFull_.wait(lock, [this]{return count_ != capacity_;});
 
-        if (closed_)
+        std::construct_at(std::addressof(buffer_[pushCursor_]), std::forward<Args>(args)...);
+
+        pushCursor_ = (pushCursor_ + 1) % capacity_;
+        count_++;
+    }
+
+    template <typename... Args>
+    bool try_emplace(Args&&... args)
+    {
+        std::scoped_lock<std::mutex> lock{mutex_};
+
+        if (count_ == capacity_)
         {
-            return;
+            return false;
         }
 
-        std::construct_at(std::addressof(buffer_[pushCursor_]), std::forward<Args>(item)...);
+        std::construct_at(std::addressof(buffer_[pushCursor_]), std::forward<Args>(args)...);
 
         pushCursor_ = (pushCursor_ + 1) % capacity_;
         count_++;
 
-        lock.unlock();
+        return true;
     }
 
-    std::optional<T> pop()
+    template <typename U>
+    requires std::is_constructible_v<T, U&&>
+    void push(U&& value)
+    {
+        emplace(std::forward<U>(value));
+    }
+
+    template <typename U>
+    requires std::is_constructible_v<T, U&&>
+    bool try_push(U&& value)
+    {
+        return try_emplace(std::forward<U>(value));
+    }
+
+    // Same contract and API as SPSC - returns pointer to first consumable element
+    // NOT safe for multi-consumer use due to pointer access to potentially shared data
+    T* front()
+    {
+        std::scoped_lock<std::mutex> lock{mutex_};
+
+        if (count_ == 0)
+        {
+            return nullptr;
+        }
+
+        return std::addressof(buffer_[popCursor_]);
+    }
+
+    // Safe for multi-consumer use
+    // Returns std::nullopt on empty pops
+    std::optional<T> try_pop()
     {
         std::unique_lock<std::mutex> lock{mutex_};
 
-        std::optional<T> ret;
+        std::optional<T> value;
 
         if (count_ > 0)
         {
-            ret = std::move(buffer_[popCursor_]);
+            value = std::move(buffer_[popCursor_]);
             std::destroy_at(std::addressof(buffer_[popCursor_]));
 
             popCursor_ = (popCursor_ + 1) % capacity_;
@@ -76,18 +126,37 @@ public:
             cvFull_.notify_one();
         }
         
-        return ret;
+        return value;
+    }
+
+    // Same contract as SPSC for benchmarking
+    // Assumes front() has been called to prevent pop on empty
+    // NOT safe for multi-consumer use
+    void pop()
+    {
+        std::unique_lock<std::mutex> lock{mutex_};
+
+        assert(count_ > 0);
+
+        std::destroy_at(std::addressof(buffer_[popCursor_]));
+    
+        popCursor_ = (popCursor_ + 1) % capacity_;
+        count_--;
+
+        lock.unlock();
+        cvFull_.notify_one();
+    }
+
+    std::size_t capacity() const
+    {
+        return capacity_;
     }
 
     void close()
     {
-        std::unique_lock<std::mutex> lock{mutex_};
+        std::scoped_lock<std::mutex> lock{mutex_};
 
         closed_ = true;
-
-        lock.unlock();
-
-        cvFull_.notify_all();
     }
 
     bool closed() const
@@ -95,18 +164,6 @@ public:
         std::scoped_lock<std::mutex> lock{mutex_};
 
         return closed_;
-    }
-
-    bool empty() const
-    {
-        std::scoped_lock<std::mutex> lock{mutex_};
-
-        return count_ == 0;
-    }
-
-    std::size_t capacity() const
-    {
-        return capacity_;
     }
 
 private:
